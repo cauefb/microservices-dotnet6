10c05edb006c8f85f11760b3878174cc17c4befb
Cauê Ferreira <kau3hc@hotmail.com>
2022-01-01 18:09:09 -0300

    sessão 9

---
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType
+            = new MediaTypeHeaderValue("application/json");
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode) throw
+                     new ApplicationException($"Something went wrong calling the API: "+
+                     $"{response.ReasonPhrase}");
+
+            var dataAsSting = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsSting, 
+                new JsonSerializerOptions
+                { PropertyNameCaseInsensitive= true});
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(
+            this HttpClient httpClient,
+            string url,
+            T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(
+            this HttpClient httpClient,
+            string url,
+            T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
+        }
+    }
+}
